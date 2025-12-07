a619bac8108425a8547a558ba6ec8e422e16dbd4
Michal Ryšavý <mrysavy@users.noreply.github.com>
2025-12-07 22:05:10 +0100

    Aktualizovat pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
+let s: PlanetX_Display.Strip = null
+let status_red: boolean = false
+let status_yellow: boolean = false
+let status_green: boolean = false
+
 enum Color {
     Red,
     Yellow,
@@ -7,22 +12,44 @@ enum State {
     On,
     Off
 }
-enum J {
-    J1,
-    J2,
-    J3,
-    J4
-}
 //% color="#AA278D" weight=100
-namespace neopixel_semaphore {
+namespace Semaphore {
     //% block
-    export function set(j: J, color: Color, state: State) {
-
+    export function set(pin: PlanetX_Display.DigitalRJPin, color: Color, state: State) {
+        switch (color) {
+            case Color.Red:
+                status_red = (state == State.On)
+                break;
+            case Color.Yellow:
+                status_yellow = (state == State.On)
+                break;
+            case Color.Green:
+                status_green = (state == State.On)
+                break;
+        }
+        redraw(pin)
     }
 
     //% block
-    export function clear(j: J) {
-
+    export function clear(pin: PlanetX_Display.DigitalRJPin) {
+        status_red = false
+        status_yellow = false
+        status_green = false
+        redraw(pin)
     }
 
+    function redraw(pin: PlanetX_Display.DigitalRJPin) {
+        s = PlanetX_Display.create(pin, 8, PlanetX_Display.NeoPixelMode.RGB)
+        s.clear()
+        if (status_red) {
+            s.setPixelColor(7, PlanetX_Display.colors(PlanetX_Display.NeoPixelColors.Red))
+        }
+        if (status_yellow) {
+            s.setPixelColor(0, PlanetX_Display.colors(PlanetX_Display.NeoPixelColors.Yellow))
+        }
+        if (status_green) {
+            s.setPixelColor(1, PlanetX_Display.colors(PlanetX_Display.NeoPixelColors.Green))
+        }
+        s.show()
+    }
 }
