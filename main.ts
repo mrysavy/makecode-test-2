73e82fcd0e864f501bebd912fa122d490c988df6
Michal Ryšavý <mrysavy@users.noreply.github.com>
2025-12-07 12:39:32 +0100

    Aktualizovat pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,31 @@
+enum Color {
+    Red,
+    Yellow,
+    Green
+}
 
+enum State {
+    On,
+    Off
+}
+
+enum J {
+    J1,
+    J2,
+    J3,
+    J4
+}
+
+//% color="#AA278D" weight=100
+namespace neopixel_semaphore {
+    //% block
+    export function set(j: J, color: Color, state: State) {
+
+    }
+
+    //% block
+    export function clear(j: J) {
+
+    }
+
+}
